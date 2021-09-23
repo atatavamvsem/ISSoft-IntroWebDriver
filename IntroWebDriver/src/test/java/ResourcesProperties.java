bd1d1926de178102dabc058dbc9ae4ca38ec0e1d
AntonGubko <AntonGubko@coherentsolutions.com>
2021-09-23 15:26:22 +0300

    Test implemented

---
+++ IntroWebDriver/src/test/java/ResourcesProperties.java
@@ -0,0 +1,39 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ResourcesProperties {
+    private static FileInputStream fileConfigInputStream;
+    private static FileInputStream fileTestDataInputStream;
+    private static Properties CONF_PROPERTIES;
+    private static Properties DATA_PROPERTIES;
+
+    static {
+        try {
+            fileConfigInputStream = new FileInputStream("src/test/resources/credentials.properties");
+            fileTestDataInputStream = new FileInputStream("src/test/resources/testData.properties");
+            CONF_PROPERTIES = new Properties();
+            DATA_PROPERTIES = new Properties();
+            CONF_PROPERTIES.load(fileConfigInputStream);
+            DATA_PROPERTIES.load(fileTestDataInputStream);
+        } catch (IOException e) {
+            throw new UnsupportedOperationException(e);
+        } finally {
+            if (fileConfigInputStream != null && fileTestDataInputStream != null)
+                try {
+                    fileConfigInputStream.close();
+                    fileTestDataInputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+        }
+    }
+
+    public static String getCredProperty(String key) {
+        return CONF_PROPERTIES.getProperty(key);
+    }
+
+    public static String getDataProperty(String key) {
+        return DATA_PROPERTIES.getProperty(key);
+    }
+}
---
+++ IntroWebDriver/src/test/java/YandexTest.java
@@ -0,0 +1,55 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+public class YandexTest {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @BeforeEach
+    public void setUp() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        wait = new WebDriverWait(driver,30);
+    }
+
+    @Test
+    public void loginTest(){
+        driver.get("https://mail.yandex.com/");
+
+        Assertions.assertTrue(driver.getTitle().contains(ResourcesProperties.getDataProperty("startPageTitle")), "It's wrong page");
+
+        WebElement inputButton = driver.findElement(By.xpath("//a[contains(@class,'HeadBanner-Button-Enter')]"));
+        inputButton.click();
+
+        Assertions.assertEquals(ResourcesProperties.getDataProperty("authorizationPageTitle"), driver.getTitle(), "It's wrong page");
+
+        WebElement loginInput = driver.findElement(By.xpath("//input[@id='passp-field-login']"));
+        loginInput.sendKeys(ResourcesProperties.getCredProperty("login"));
+
+        WebElement loginButton = driver.findElement(By.xpath("//button[@id='passp:sign-in']"));
+        loginButton.click();
+
+        WebElement passwordInput = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@id='passp-field-passwd']")));
+        passwordInput.sendKeys(ResourcesProperties.getCredProperty("password"));
+
+        WebElement loginSecondButton = driver.findElement(By.xpath("//button[@id='passp:sign-in']"));
+        loginSecondButton.click();
+
+        Assertions.assertTrue(wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@class='PSHeaderLogo360-360']"))).isDisplayed(), "It's wrong page");
+    }
+
+    @AfterEach
+    public void closeUp() {
+        driver.quit();
+    }
+}
